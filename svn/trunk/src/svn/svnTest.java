75ec4a8847a3947b38dba967d90dad50a57ba2cb
nerd09 <nerd09@338d300a-3e30-0510-9af2-baee979c18fa>
2016-04-12 00:10:18 +0000

    svn test code

---
+++ svn/trunk/src/svn/svnTest.java
@@ -0,0 +1,10 @@
+package svn;
+
+public class svnTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("SVN server for doubld J team project");
+	}
+
+}
