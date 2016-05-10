f65f1147d39a505b608b4ea44161bd699e1f111a
nerd09 <nerd09@338d300a-3e30-0510-9af2-baee979c18fa>
2016-05-10 05:36:14 +0000

    JListForm modifier : 최새롬 created date : 20160509 modified date : 20160510

--- ClassManagerProject/src/ClassManagerGUI/JListForm.java
+++ ClassManagerProject/src/ClassManagerGUI/JListForm.java
@@ -2,45 +2,23 @@ package ClassManagerGUI;
 
 import java.awt.Color;
 import java.awt.Component;
+import java.awt.GridLayout;
 
 import javax.swing.BoxLayout;
-import javax.swing.DefaultListModel;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
-import javax.swing.JList;
 import javax.swing.JPanel;
-import javax.swing.ListCellRenderer;
 import javax.swing.border.LineBorder;
-
-public class JListForm<T> extends JList<T> {
-	static DefaultListModel model= new DefaultListModel();
+public class JListForm extends JPanel {
 	
-	public JListForm() {
-		super(model);
-		super.setCellRenderer(new ListCellRenderer<T>(){
-			@Override
-		    public Component getListCellRendererComponent(JList list, Object value, int index, boolean isSelected, boolean cellHasFocus) {
-		        JPanel renderer = (JPanel) value;
-		        return renderer;
-		    }
-		});
+	JListForm(){
+		setLayout(new GridLayout(0,1,0,0));
+		this.setOpaque(false);
+		this.setBorder(null);
 	}
 	
-	public void addComponent(T element){
-		model.addElement(element);
+	public void addComponent(Component element){
+		add(element);
 	}
-}
-
-class EventPanel extends JPanel {
-	String[] event = { "커뮤니티", "캘린더", "공지" };
-
-	EventPanel(int eventType, String eventContent) {
-		setBorder(new LineBorder(new Color(128, 128, 128), 1, true));
-		setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
-		JLabel lblType = new JLabel(event[eventType]);
-		lblType.setBorder(new LineBorder(new Color(0, 0, 0), 1, true));
-		add(lblType);
-		JLabel lblContent = new JLabel(eventContent);
-		add(lblContent);
-	}
+	
 }
