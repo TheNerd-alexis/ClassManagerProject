758a39415914001a2223b1b8d434bbb7e2ade92c
fb_old <fb_old@338d300a-3e30-0510-9af2-baee979c18fa>
2016-05-18 09:45:10 +0000

    2016-05-18 EventService CMResult Version

--- ClassManagerProject/src/Service/EventService.java
+++ ClassManagerProject/src/Service/EventService.java
@@ -3,69 +3,87 @@ package Service;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 import DAO.EventDAO;
+import DAO.GetDAO;
 import Model.AbstractModel;
+import Model.CMResult;
 import Model.Event;
 
 public class EventService {
-	
-	private final static String URL = "jdbc:mysql://localhost:3306/classmanager";
-	private final static String USER = "root";
-	private final static String PASSWORD = "mysql";
-	Connection connection;
-	EventDAO eventDao;
-	
-	public EventService() {
-		try {
-			Class.forName("com.mysql.jdbc.Driver");
-			connection = DriverManager.getConnection(URL, USER, PASSWORD);
-			eventDao = EventDAO.getInstance(connection);
-		} catch (ClassNotFoundException e) {
-			// TODO Auto-generated catch block
-			System.out.println("클래스 적재 실패 com.mysql/jdbc.Driver !");
-		}
-		
-		catch (SQLException e) {
-			// TODO Auto-generated catch block
-			System.out.println("SQL 연결 실패!");
-		}		
-	}
-	
-	
-	public List<Event> event_refresh(AbstractModel model) {
-		
-		Event tempEvent = (Event) model;
-		return  eventDao.selectEvent(tempEvent);
-	}
-	
-	public int event_add(AbstractModel model) {
-		
-		Event tempEvent = (Event) model;
 
-		List<Event> listEvent = eventDao.selectEvent(tempEvent);
+	static GetDAO dao = GetDAO.getInstance();
+		
+	public static CMResult event_refresh(AbstractModel model) {
+		
+		Event tempEvent = (Event) model;
+		CMResult result = new CMResult();
+		
+		List<AbstractModel> resultList = new ArrayList<AbstractModel>();
+		for(AbstractModel m : dao.getEventDao().selectEvent(tempEvent)) {
+			resultList.add(m);
+		}
+		result.setResultList( resultList );
+		return result;
+	}
+	
+	/**
+	 * 이벤트 추가 서비스
+	 * @param model = 이벤트 추가시킬 MODEL
+	 * @return 0 = DB에서 찾을 수 없음
+	 *         + = 이벤트 추가 성공
+	 *         -1 = 이벤트 추가 실패 (PRIMARY KEY 중복)
+	 */
+	public static CMResult event_add(AbstractModel model) {
+		
+		Event tempEvent = (Event) model;
+		CMResult result = new CMResult();
+		
+		List<Event> listEvent = dao.getEventDao().selectEvent(tempEvent);
 		
 		if (listEvent.size() > 0) {
 			for(Event el : listEvent) {
 				if(tempEvent.getMid() == el.getMid() && tempEvent.getEtitle().equals(el.getEtitle())) {
-					return 3;
+					result.setResult(-1);
+					return result;
 				}
 			}
 		}
-		return eventDao.insertEvent(tempEvent);
+		result.setResult( dao.getEventDao().insertEvent(tempEvent) );
+		return result;
 	}
 	
-	public int event_delete(AbstractModel model) {
+	
+	/**
+	 * 이벤트 삭제 서비스
+	 * @param model = 이벤트 삭제시킬 MODEL
+	 * @return 0 = DB에서 찾을 수 없음
+	 *         + = 이벤트 삭제 성공
+	 *         - = 이벤트 삭제 실패
+	 */
+	public static CMResult event_delete(AbstractModel model) {
 		
 		Event tempEvent = (Event) model;
-		return eventDao.deleteEvent(tempEvent);
+		CMResult result = new CMResult();
+		result.setResult( dao.getEventDao().deleteEvent(tempEvent) );
+		return result;
 	}
 	
-	public int event_edit(AbstractModel newModel, AbstractModel editModel) {
+	/**
+	 * 이벤트 수정 서비스
+	 * @param newModel = 이벤트 수정값 데이터 MODEL
+	 * @param editModel = 이벤트 수정시킬 데이터 MODEL
+	 * @return 0 = DB에서 찾을 수 없음
+	 *         + = 이벤트 수정 성공
+	 *         - = 이벤트 수정 실패
+	 */
+	public static CMResult event_edit(AbstractModel newModel, AbstractModel editModel) {
 		Event newEvent = (Event) newModel;
 		Event editEvent = (Event) editModel;
-		
-		return eventDao.updateEvent(newEvent, editEvent);
+		CMResult result = new CMResult();
+		result.setResult( dao.getEventDao().updateEvent(newEvent, editEvent) );
+		return result;
 	}
 }
