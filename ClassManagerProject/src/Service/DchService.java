01173a2a96f6ec4e7e7001c1a1da2638b9ffb92f
fb_old <fb_old@338d300a-3e30-0510-9af2-baee979c18fa>
2016-05-19 13:06:03 +0000

    2016-05-19 DchService Update V3

--- ClassManagerProject/src/Service/DchService.java
+++ ClassManagerProject/src/Service/DchService.java
@@ -2,53 +2,118 @@ package Service;
 
 import java.sql.Connection;
 import java.sql.Date;
+import java.util.ArrayList;
 import java.util.List;
 
 import DAO.DchDAO;
+import DAO.GetDAO;
 import Model.AbstractModel;
+import Model.CMResult;
 import Model.Dch;
 
 //일일 출첵 작성
 
 public class DchService {
 
-	private Connection connection;
-	DchDAO dao = DchDAO.getInstance(connection);
+	static GetDAO dao = GetDAO.getInstance();
+	
+	/* Null Type:
+	 * -100: MID 외래키(MID)
+	 * -101: ATTENDDATE
+	 * -102: ATTENDANCE
+	 * -103: startDate
+	 * -104: endDate
+	 */
 
 	// 출첵 정보 입력
-	public int dch_add(AbstractModel model) {
+	/**
+	 * 출석체크 추가 서비스
+	 * @param model = Dch
+	 * @return 1: 출석체크 추가 성공<br>
+	 *         0: 출석체크 추가 실패(INSERT 실패)<br>
+	 *        -1: SQL Exception<br>
+	 *        -2: List<Dch> 조회 실패<br>
+	 *        -100: MID - NULL<br>
+	 */
+	public static CMResult dch_add(AbstractModel model) {
 		Dch dch = (Dch) model;
+		CMResult result = new CMResult();
 
 		if (dch.getMID() == null)
-			return 3;
-		if (dch.getATTENDDATE() == null)
-			return 4;
+			return result.setResult(-100);
+		
+		List<Dch> dchList = dao.getDchDao().selectDch(dch);
+		if (dchList.size() < 1)
+			return result.setResult(-2);
 
-		return dao.insertDch(dch);
+		List<AbstractModel> resultList = new ArrayList<AbstractModel>();
+		for(Dch d : dchList) {
+			resultList.add(d);
+		}
+		result.setResultList(resultList);
+		return result.setResult( dao.getDchDao().insertDch(dch));
 	}
 	
 	// 일일 출첵 수정
-	public int dch_update(AbstractModel model) {
-		Dch dch = (Dch) model;
+	/**
+	 * 출석체크 수정 서비스
+	 * @param model = Dch
+	 * @return 1: 출석체크 수정 성공<br>
+	 *         0: 출석체크 수정 실패(UPDATE 실패)<br>
+	 *        -1: SQL Exception<br>
+	 *        -2: List<Dch> 조회 실패<br>
+	 *        -100: MID - NULL<br>
+	 *        -101: ATTENDATE - NULL<br>
+	 *        -102: ATTENDANCE - NULL
+	 */
+	public CMResult dch_update(AbstractModel newModel, AbstractModel beforeModel) {
+		Dch newDch = (Dch) newModel;
+		Dch beforeDch = (Dch) beforeModel;
+		CMResult result = new CMResult();
 		
-		if (dch.getMID() == null)
-			return 3;
-		if (dch.getATTENDDATE() == null)
-			return 4;
+		if (newDch.getMID() == null)
+			return result.setResult(-100);
 		
-//		dao.update(새로운 정보(mid, date, 출석여부))
-		return dao.updateDch(dch, dch);
+		if (newDch.getATTENDDATE() == null)
+			return result.setResult(-101);
+		
+		if (newDch.getATTENDANCE() == null)
+			return result.setResult(-102);
+		
+		return result.setResult( dao.getDchDao().updateDch(newDch, beforeDch) );
 	}
 
 	// 출첵 목록 조회
-	public List<Dch> getDchList(AbstractModel model, Date startDate, Date endDate) {
+	/**
+	 * 출석체크 목록 조회 서비스
+	 * @param model = Dch
+	 * @return 1: 출석체크 목록 조회 성공<br>
+	 *         0: 출석체크 목록 조회 실패(SELECT(Date) 실패)<br>
+	 *        -1: SQL Exception<br>
+	 *        -2: List<Dch> 조회 실패<br>
+	 *        -103: startDate - NULL<br>
+	 *        -104: endDate - NULL
+	 */
+	public CMResult getDchList(AbstractModel model, Date startDate, Date endDate) {
 		Dch dch = (Dch) model;
+		CMResult result = new CMResult();
 		
-		return dao.selectDch(dch, startDate, endDate);
+		if (startDate == null)
+			return result.setResult(-103);
+		
+		if (endDate == null)
+			return result.setResult(-104);
+			
+		
+		List<Dch> dchList = dao.getDchDao().selectDch(dch, startDate, endDate);
+		if(dchList.size() < 1)
+			return result.setResult(-2);
+				
+		List<AbstractModel> resultList = new ArrayList<AbstractModel>();
+		for(Dch d : dchList) {
+			resultList.add(d);
+		}
+		result.setResultList(resultList);
+		return result.setResult(1);
 	}
-	
-	
-
-	
-	
 }
