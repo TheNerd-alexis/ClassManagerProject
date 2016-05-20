1ddad95f3a0e200c2dcd89b158a3b4c13fdb27d9
fb_old <fb_old@338d300a-3e30-0510-9af2-baee979c18fa>
2016-05-20 03:49:55 +0000

    2016-05-20 EventService Update V3

--- ClassManagerProject/src/Service/EventService.java
+++ ClassManagerProject/src/Service/EventService.java
@@ -15,124 +15,90 @@ import Model.Event;
 public class EventService {
 
 	static GetDAO dao = GetDAO.getInstance();
+	
+	/*
+	 * Null Type:
+	 * -100: MID 외래키(MID)(PK)
+	 * -101: ETYPE
+	 * -102: ETITLE(PK)
+	 * -103: ESTATUS
+	 * 
+	 * -200: (EDIT) MID
+	 * -201: (EDIT) ETITLE
+	 */
 		
 	public static CMResult event_refresh(AbstractModel model) {
 		
-		Event tempEvent = (Event) model;
+		Event event = (Event) model;
 		CMResult result = new CMResult();
 		
+		List<Event> listEvent = dao.getEventDao().selectEvent(event);
+		if (listEvent.size() < 1)
+			return result.setResult(-2);
+		
 		List<AbstractModel> resultList = new ArrayList<AbstractModel>();
-		for(AbstractModel m : dao.getEventDao().selectEvent(tempEvent)) {
+		for(AbstractModel m : listEvent) {
 			resultList.add(m);
 		}
 		
-		if (resultList.size() < 1) {
-			result.setResult(-1);
-		}
-		else {
-			result.setResultList( resultList );
-			result.setResult(1);
-		}
-		return result;
+		result.setResultList( resultList );
+		return result.setResult(1);
 	}
-	
-	/**
-	 * 이벤트 추가 서비스
-	 * @param model = 이벤트 추가시킬 MODEL
-	 * @return 0 = DB에서 찾을 수 없음
-	 *         + = 이벤트 추가 성공
-	 *         -1 = 이벤트 추가 실패 (PRIMARY KEY 중복)
-	 */
+
 	public static CMResult event_add(AbstractModel model) {
 		
 		Event event = (Event) model;
 		CMResult result = new CMResult();
 		
-		if (event.getMid() == null) {
-			result.setResult(-2);
-		}
-		else if (event.getEtype() == null) {
-			result.setResult(-3);
-		}
-		else if (event.getEtitle() == null) {
-			result.setResult(-4);
-		}
-		else if (event.getEstatus() == null) {
-			result.setResult(-5);
-		} else {
-			List<Event> listEvent = dao.getEventDao().selectEvent(event);
-			
-			if (listEvent.size() > 0) {
-				for(Event el : listEvent) {
-					if(event.getMid() == el.getMid() && event.getEtitle().equals(el.getEtitle())) {
-						result.setResult(-1);
-						return result;
-					}
-				}
-			}
-			result.setResult( dao.getEventDao().insertEvent(event) );
-		}
-		return result;
+		if (event.getMid() == null)
+			return result.setResult(-100);
+		
+		if (event.getEtype() == null)
+			return result.setResult(-101);
+		
+		if (event.getEtitle() == null)
+			return result.setResult(-102);
+		
+		if (event.getEstatus() == null)
+			return result.setResult(-103);
+				
+		return result.setResult( dao.getEventDao().insertEvent(event) );
 	}
 	
 	
-	/**
-	 * 이벤트 삭제 서비스
-	 * @param model = 이벤트 삭제시킬 MODEL
-	 * @return 0 = DB에서 찾을 수 없음
-	 *         + = 이벤트 삭제 성공
-	 *         - = 이벤트 삭제 실패
-	 */
 	public static CMResult event_delete(AbstractModel model) {
 		
 		Event event = (Event) model;
 		CMResult result = new CMResult();
 		
-		if (event.getMid() == null) {
-			result.setResult(-2);
-		}
-		else if (event.getEtype() == null) {
-			result.setResult(-3);
-		}
-		else if (event.getEtitle() == null) {
-			result.setResult(-4);
-		}
-		else if (event.getEstatus() == null) {
-			result.setResult(-5);
-		} else {
-			result.setResult( dao.getEventDao().deleteEvent(event) );
-		}
-		return result;
+		if (event.getMid() == null)
+			return result.setResult(-100);
+		
+		if (event.getEtitle() == null)
+			return result.setResult(-102);
+			
+		return result.setResult( dao.getEventDao().deleteEvent(event) );
 	}
 	
-	/**
-	 * 이벤트 수정 서비스
-	 * @param newModel = 이벤트 수정값 데이터 MODEL
-	 * @param editModel = 이벤트 수정시킬 데이터 MODEL
-	 * @return 0 = DB에서 찾을 수 없음
-	 *         + = 이벤트 수정 성공
-	 *         - = 이벤트 수정 실패
-	 */
+
 	public static CMResult event_edit(AbstractModel newModel, AbstractModel editModel) {
 		Event newEvent = (Event) newModel;
 		Event editEvent = (Event) editModel;
 		
 		CMResult result = new CMResult();
 		
-		if (newEvent.getMid() == null) {
-			result.setResult(-2);
-		}
-		else if (newEvent.getEtype() == null) {
-			result.setResult(-3);
-		}
-		else if (newEvent.getEtitle() == null) {
-			result.setResult(-4);
-		}
-		else if (newEvent.getEstatus() == null) {
-			result.setResult(-5);
-		} else {
-			result.setResult( dao.getEventDao().updateEvent(newEvent, editEvent) );
-		}
-		return result;
+		if (newEvent.getMid() == null)
+			result.setResult(-100);
+		
+		if (newEvent.getEtitle() == null)
+			result.setResult(-102);
+		
+		if (editEvent.getMid() == null)
+			result.setResult(-200);
+		
+		if (editEvent.getEtitle() == null)
+			result.setResult(-201);
+		
+		return result.setResult( dao.getEventDao().updateEvent(newEvent, editEvent) );
 	}
 }
